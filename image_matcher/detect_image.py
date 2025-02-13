edca705b9fc8efb86530f3f67a35b7f29790c57c
Eric Heiden <eric.heiden@gmail.com>
2025-02-12 21:25:50 -0500

    fixed some variable name changes (cnts -> contours)

--- image_matcher/detect_image.py
+++ image_matcher/detect_image.py
@@ -197,20 +197,20 @@ def find_contours(image, ksize=3, # Smooth out noise but keep card edges
     index_sort = sorted(range(len(contours)), key=lambda i : cv2.contourArea(contours[i]),reverse=True)
 
     # If there are no contours, do nothing
-    if len(cnts) == 0:
+    if len(contours) == 0:
         return [], []
     
     # Otherwise, initialize empty sorted contour and hierarchy lists
     cnts_sort = []
     hier_sort = []
-    cnt_is_card = np.zeros(len(cnts),dtype=int)
+    cnt_is_card = np.zeros(len(contours),dtype=int)
 
     # Fill empty lists with sorted contour and sorted hierarchy. Now,
     # the indices of the contour list still correspond with those of
     # the hierarchy list. The hierarchy array can be used to check if
     # the contours have parents or not.
     for i in index_sort:
-        cnts_sort.append(cnts[i])
+        cnts_sort.append(contours[i])
         hier_sort.append(hier[0][i])
 
     # Determine which of the contours are cards by applying the
@@ -267,76 +267,6 @@ def _is_rectangular(contour):
 def _threshold_size_bounded_by(area, min_area=5000, max_area=200000):
     print(f"Contour area: {area}")
     return min_area <= area <= max_area
-
-
-def preprocess_card(contour, image):
-    """Uses contour to find information about the query card. Isolates rank
-    and suit images from the card."""
-
-    # Initialize new Query_card object
-    qCard = Query_card()
-
-    qCard.contour = contour
-
-    # Find perimeter of card and use it to approximate corner points
-    peri = cv2.arcLength(contour,True)
-    approx = cv2.approxPolyDP(contour,0.01*peri,True)
-    pts = np.float32(approx)
-    qCard.corner_pts = pts
-
-    # Find width and height of card's bounding rectangle
-    x,y,w,h = cv2.boundingRect(contour)
-    qCard.width, qCard.height = w, h
-
-    # Find center point of card by taking x and y average of the four corners.
-    average = np.sum(pts, axis=0)/len(pts)
-    cent_x = int(average[0][0])
-    cent_y = int(average[0][1])
-    qCard.center = [cent_x, cent_y]
-
-    # Warp card into 200x300 flattened image using perspective transform
-    qCard.warp = flattener(image, pts, w, h)
-
-    # Grab corner of warped card image and do a 4x zoom
-    Qcorner = qCard.warp[0:CORNER_HEIGHT, 0:CORNER_WIDTH]
-    Qcorner_zoom = cv2.resize(Qcorner, (0,0), fx=4, fy=4)
-
-    # Sample known white pixel intensity to determine good threshold level
-    white_level = Qcorner_zoom[15,int((CORNER_WIDTH*4)/2)]
-    thresh_level = white_level - CARD_THRESH
-    if (thresh_level <= 0):
-        thresh_level = 1
-    retval, query_thresh = cv2.threshold(Qcorner_zoom, thresh_level, 255, cv2. THRESH_BINARY_INV)
-    
-    # Split in to top and bottom half (top shows rank, bottom shows suit)
-    Qrank = query_thresh[20:185, 0:128]
-    Qsuit = query_thresh[186:336, 0:128]
-
-    # Find rank contour and bounding rectangle, isolate and find largest contour
-    dummy, Qrank_cnts, hier = cv2.findContours(Qrank, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    Qrank_cnts = sorted(Qrank_cnts, key=cv2.contourArea,reverse=True)
-
-    # Find bounding rectangle for largest contour, use it to resize query rank
-    # image to match dimensions of the train rank image
-    if len(Qrank_cnts) != 0:
-        x1,y1,w1,h1 = cv2.boundingRect(Qrank_cnts[0])
-        Qrank_roi = Qrank[y1:y1+h1, x1:x1+w1]
-        Qrank_sized = cv2.resize(Qrank_roi, (RANK_WIDTH,RANK_HEIGHT), 0, 0)
-        qCard.rank_img = Qrank_sized
-
-    # Find suit contour and bounding rectangle, isolate and find largest contour
-    dummy, Qsuit_cnts, hier = cv2.findContours(Qsuit, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    Qsuit_cnts = sorted(Qsuit_cnts, key=cv2.contourArea,reverse=True)
-    
-    # Find bounding rectangle for largest contour, use it to resize query suit
-    # image to match dimensions of the train suit image
-    if len(Qsuit_cnts) != 0:
-        x2,y2,w2,h2 = cv2.boundingRect(Qsuit_cnts[0])
-        Qsuit_roi = Qsuit[y2:y2+h2, x2:x2+w2]
-        Qsuit_sized = cv2.resize(Qsuit_roi, (SUIT_WIDTH, SUIT_HEIGHT), 0, 0)
-        qCard.suit_img = Qsuit_sized
-
-    return qCard
 
 def preprocess_image(image, thresh_max_value=255, block_size=101, offset_c=10, kernel_size=(5,5), stddev=0):
     """Returns a grayed, blurred, and adaptively thresholded camera image."""
