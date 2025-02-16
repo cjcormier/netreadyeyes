warping rectangular box to be the correct orientation and rotation now helps identification - still not perfect though

image_matcher/detect_image.py
@@ -24,78 +24,95 @@ def find_cards(query_image,
     if display_mode == "thresholding" and display_image:
         display_image(thresh_image)
 
-    card_sized_imgs = extract_card_bounding_boxes(query_image, thresh_image, 
-                                                  rec_params,
-                                                  display_mode, display_image)
+    #mode = cv2.RETR_EXTERNAL
+    # mode = cv2.RETR_LIST
+    mode = cv2.RETR_CCOMP
+    # mode = cv2.RETR_TREE
 
-    # rotation_corrected_imgs = preprocess_rotate(card_sized_imgs)
+    # Find contours
+    contours,hier = cv2.findContours(thresh_image,mode,cv2.CHAIN_APPROX_SIMPLE)
 
-    print(f"found {len(card_sized_imgs)} card sized boxes in this image")
-    
-    for candidate in card_sized_imgs
-        card, diff = find_minimum_hash_difference(candidate, hash_pool)
+    # display contours that we found
+    if display_mode == "unfiltered contours" and display_image:
+        image_contours = query_image.copy()
+        image_rgb = cv2.cvtColor(image_contours, cv2.COLOR_BGR2RGB)
+        cv2.drawContours(image_rgb, contours, -1, (0, 255, 0), 2)
+        display_image(image_rgb)
+
+    # filter the contours based on the area and area-to-perimeter ratio 
+    # (cards luckily have a consistent shape)
+    # filtered_contours = filter_contours(contours)
+    filtered_contours = find_rectangular_contours(contours, hier)
+    card_sized_contours = filter_contour_size(filtered_contours,
+                                              rec_params['card_min_area'],
+                                              rec_params['card_max_area'])
+
+    # display filtered contours
+    if display_mode == "filtered contours" and display_image:
+        image_filtered_contours = query_image.copy()
+        image_rgb = cv2.cvtColor(image_filtered_contours, cv2.COLOR_BGR2RGB)
+        cv2.drawContours(image_rgb, card_sized_contours, -1, (0, 255, 0), 2)
+        display_image(image_rgb)
+
+    print(f"found {len(card_sized_contours)} card sized boxes in this image")
+
+    for n, contour in enumerate(card_sized_contours):
+        
+        rectangle_points = _get_rectangle_points_from_contour(contour)
+        card_image = _four_point_transform(query_image, rectangle_points)
+        card, diff = find_minimum_hash_difference(card_image, hash_pool)
         if _possible_match(diff):
-
             print(f"find_minimum_hash_difference returned {card['name']} with a diff of {diff}.")
-            
-            # # Display the image using OpenCV
-            # cv2.imshow(card['name'], card_image)
-
-            # # Wait for a key press to close the window
-            # cv2.waitKey(0)
-            # # Close all OpenCV windows
-            # cv2.destroyAllWindows()
-
-            # # Unpack card path and diff from find_minimum_hash_difference
-            # min_dist_df, diff =  (candidate, hash_pool)
-
             # Send the result back to the recognition_queue
             recognition_queue.put(card['name']) 
 
-        # cv2.imshow('Card Image', candidate)
-        # key = cv2.waitKey(0) & 0xFF
-        # hashes.append(compute_image_hash(candidate))
+
+    # card_sized_imgs = extract_card_bounding_boxes(query_image, thresh_image, 
+    #                                               rec_params,
+    #                                               display_mode, display_image)
+
+    # rotation_corrected_imgs = preprocess_rotate(card_sized_imgs)
 
     
 def _possible_match(diff):
     if diff < 450: #To-do: make this number based on the threshold slider
         return True
 
-def preprocess_rotate(images):
-    rotated_imgs = []
+# def preprocess_rotate(images):
+#     rotated_imgs = []
 
-    for image in images:
-        if isinstance(image, Image.Image):
-            converted_img = convert_image_to_opencv(image)
-        elif isinstance(image, np.ndarray):
-            converted_img = image
-        else:
-            return
+#     for image in images:
+#         if isinstance(image, Image.Image):
+#             converted_img = convert_image_to_opencv(image)
+#         elif isinstance(image, np.ndarray):
+#             converted_img = image
+#         else:
+#             return
         
-        # Find edges using Canny edge detection to detect orientation
-        edges = cv2.Canny(converted_img, 50, 150, apertureSize=3)
+#         # Find edges using Canny edge detection to detect orientation
+#         edges = cv2.Canny(converted_img, 50, 150, apertureSize=3)
         
-        # Find the rotation angle
-        coords = np.column_stack(np.where(edges > 0))
-        angle = cv2.minAreaRect(coords)[-1]
+#         # Find the rotation angle
+#         coords = np.column_stack(np.where(edges > 0))
+#         angle = cv2.minAreaRect(coords)[-1]
         
-        # Correct the image rotation
-        (h, w) = image.shape[:2]
-        center = (w // 2, h // 2)
-        M = cv2.getRotationMatrix2D(center, angle, 1.0)
-        rotated_image = cv2.warpAffine(converted_img, M, (w, h))
+#         # Correct the image rotation
+#         (h, w) = image.shape[:2]
+#         center = (w // 2, h // 2)
+#         M = cv2.getRotationMatrix2D(center, angle, 1.0)
+#         rotated_image = cv2.warpAffine(converted_img, M, (w, h))
 
-        # # Display the image using OpenCV
-        cv2.imshow("rotated_image", rotated_image)
+#         # # Display the image using OpenCV
+#         cv2.imshow("rotated_image", rotated_image)
 
-        # Wait for a key press to close the window
-        cv2.waitKey(0)
-        # Close all OpenCV windows
-        cv2.destroyAllWindows()
+#         # Wait for a key press to close the window
+#         cv2.waitKey(0)
+#         # Close all OpenCV windows
+#         cv2.destroyAllWindows()
 
-        rotated_imgs.append(convert_image_to_pil(rotated_image))
+#         rotated_imgs.append(convert_image_to_pil(rotated_image))
 
-    return rotated_imgs
+#     return rotated_imgs
 
 
 def find_rectangular_contours(contours, hierarchy):
@@ -134,6 +151,108 @@ def _get_stack(hierarchy):
     ]
 
 
+def _get_rectangle_points_from_contour(contour):
+    return np.float32([p[0] for p in contour])
+
+
+def _four_point_transform(image, pts, for_display=False):
+    """Transform a quadrilateral section of an image into a rectangular area.
+
+    Parameters
+    ----------
+    image : Image
+        source image
+    pts : np.array
+
+    Returns
+    -------
+    Image
+        Transformed rectangular image
+    """
+
+    rect = _order_points(pts)
+
+    spacing_around_card = 0
+    double_spacing_around_card = 0
+    if for_display:
+        spacing_around_card = 100
+        double_spacing_around_card = 2 * spacing_around_card
+
+    max_height, max_width = _get_edges(double_spacing_around_card, rect)
+    transformed_image = _warp_image(image, max_height, max_width, rect,
+                                    spacing_around_card)
+    if _image_is_horizontal(max_width, max_height):
+        transformed_image = rotate_image(max_height, max_width, transformed_image)
+    return transformed_image
+
+
+def _order_points(pts):
+    """Initialize a list of coordinates that will be ordered such that the first entry in the list is the top-left,
+        the second entry is the top-right, the third is the bottom-right, and the fourth is the bottom-left.
+
+    Parameters
+    ----------
+    pts : np.array
+
+    Returns
+    -------
+    : ordered list of 4 points
+    """
+
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]  # the top-left point will have the smallest sum, whereas
+    rect[2] = pts[np.argmax(s)]  # the bottom-right point will have the largest sum
+
+    diff = np.diff(pts, axis=1)     # now, compute the difference between the points, the
+    rect[1] = pts[np.argmin(diff)]  # top-right point will have the smallest difference,
+    rect[3] = pts[np.argmax(diff)]  # whereas the bottom-left will have the largest difference
+    return rect
+
+
+def _get_edges(double_spacing_around_card, rect):
+    (tl, tr, br, bl) = rect
+    max_width = max(int(_get_edge(bl, br)), int(_get_edge(tl, tr)))
+    max_width += double_spacing_around_card
+    max_height = max(int(_get_edge(br, tr)), int(_get_edge(bl, tl)))
+    max_height += double_spacing_around_card
+    return max_height, max_width
+
+
+def _get_edge(bl, br):
+    return np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+
+
+def _warp_image(image, max_height, max_width, rect, spacing_around_card):
+    transformation_array = np.array([
+        [0 + spacing_around_card, 0 + spacing_around_card],
+        [max_width - spacing_around_card, 0 + spacing_around_card],
+        [max_width - spacing_around_card, max_height - spacing_around_card],
+        [0 + spacing_around_card, max_height - spacing_around_card]
+    ],
+        dtype="float32"
+    )
+    applied_transformation_matrix = cv2.getPerspectiveTransform(rect,
+                                                                transformation_array)
+    warped_matrix = cv2.warpPerspective(image, applied_transformation_matrix,
+                                        (max_width, max_height))
+    return warped_matrix
+
+
+def _image_is_horizontal(max_width, max_height):
+    return max_width > max_height
+
+
+def rotate_image(max_height, max_width, transformed_image):
+    center = (max_width / 2, max_height / 2)
+    rotated_applied_transformation_matrix = cv2.getRotationMatrix2D(center, 270, 1.0)
+    transformed_image = cv2.warpAffine(transformed_image,
+                                       rotated_applied_transformation_matrix,
+                                       (max_height, max_width))
+    return transformed_image
+
+
+
 def _find_bounded_contour(contours, i_contour):
     contour = contours[i_contour]
     size = cv2.contourArea(contour)
@@ -170,37 +289,7 @@ def extract_card_bounding_boxes(image, thresh, rec_params,
                                 display_mode=None, display_image=None):
     """ Detect contours, filter them, and get bounding boxes using approxPolyDP. """
     
-     #mode = cv2.RETR_EXTERNAL
-    # mode = cv2.RETR_LIST
-    mode = cv2.RETR_CCOMP
-    # mode = cv2.RETR_TREE
-
-    # Find contours
-    contours,hier = cv2.findContours(thresh,mode,cv2.CHAIN_APPROX_SIMPLE)
-
-    #we'll want to sort their indices by contour size
-
-    # display contours that we found
-    if display_mode == "unfiltered contours" and display_image:
-        image_contours = image.copy()
-        image_rgb = cv2.cvtColor(image_contours, cv2.COLOR_BGR2RGB)
-        cv2.drawContours(image_rgb, contours, -1, (0, 255, 0), 2)
-        display_image(image_rgb)
-
-    # filter the contours based on the area and area-to-perimeter ratio 
-    # (cards luckily have a consistent shape)
-    # filtered_contours = filter_contours(contours)
-    filtered_contours = find_rectangular_contours(contours, hier)
-    card_sized_contours = filter_contour_size(filtered_contours,
-                                              rec_params['card_min_area'],
-                                              rec_params['card_max_area'])
-
-    # display filtered contours
-    if display_mode == "filtered contours" and display_image:
-        image_filtered_contours = image.copy()
-        image_rgb = cv2.cvtColor(image_filtered_contours, cv2.COLOR_BGR2RGB)
-        cv2.drawContours(image_rgb, card_sized_contours, -1, (0, 255, 0), 2)
-        display_image(image_rgb)
+     
 
     card_images = []
     